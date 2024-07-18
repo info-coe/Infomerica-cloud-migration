22aad8d3403b620644e6fbee67e52b9db01687db
info-coe <coeteam@infomericainc.in>
2024-07-18 17:37:23 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -51,11 +51,9 @@ function App() {
         </div>
       </div>
       <div className="mt-4">
-        <h1 className="text-center" style={{ color: "#FE0000" }}>
-          AWS Competency – Our Approach
-        </h1>
+        <h1 className="text-center">AWS Competency – Our Approach</h1>
         <div>
-          <div className="d-flex justify-content-center align-items-center gap-5 m-4">
+          <div className="d-md-flex justify-content-center align-items-center gap-5 m-4">
             <img
               src={AWSCompetency}
               className=""
@@ -71,7 +69,7 @@ function App() {
               optimization.
             </p>
           </div>
-          <div className="d-flex gap-3 m-4 fs-6">
+          <div className="d-md-flex gap-3 m-4 fs-6">
             <p className=" p-3 shadow rounded">
               As an AWS Select Consulting Partner, Infomerica offers
               unparalleled services and support to help clients navigate the
@@ -96,7 +94,7 @@ function App() {
         </div>
       </div>
       <div className="m-5">
-        <h1 style={{ color: "#FE0000" }}>Our AWS Service Portfolio</h1>
+        <h1>Our AWS Service Portfolio</h1>
         <p className="fs-5">
           Infomerica, where innovation meets execution. As an AWS Select
           Consulting Partner, we specialize in harnessing the sophisticated
@@ -104,7 +102,7 @@ function App() {
           meticulously crafted to meet the unique demands of the cloud
           landscape.
         </p>
-        <div className="d-flex gap-4 mt-4">
+        <div className="d-md-flex gap-4 mt-4">
           <div className="shadow ">
             <h5
               className="text-white p-3"
@@ -177,36 +175,39 @@ function App() {
         </div>
       </div>
       <div className="m-5">
-        <h1 style={{ color: "#FE0000" }}>Migration Services</h1>
+        <h1>Migration Services</h1>
         <p className="fs-5">
           Infomerica excels in guiding clients through the entire migration
           acceleration program lifecycle with expertise and ​precision.
         </p>
-        <div className="fs-5">
-          <p>
-            <b>Assess: </b>Infomerica initiates the migration journey with a
-            thorough Migration Readiness Assessment (MRA) aligned ​with the AWS
-            Cloud Adoption Framework. This assessment evaluates the enterprise's
-            cloud readiness and ​provides an action plan to address identified
-            gaps.
-          </p>
-          <p>
-            <b>Mobilise: </b>Infomerica builds AWS proficiency within client
-            organizations by facilitating hands-on migration ​experiences. This
-            phase focuses on establishing a scalable and secure AWS landing zone
-            while migrating select ​business applications to the cloud.
-          </p>
-          <p>
-            <b>Migrate & Modernise: </b>A significant portion (20-50%) of an
-            enterprise application portfolio comprises repeated ​patterns that
-            can be optimized through a factory approach. So, we're working with
-            you to set up a Migration Factory ​using patterns, processes, tools,
-            resources, and methodologies defined and tested during the
-            mobilization phase to ​migrate applications at scale. Larger and
-            more complex applications often follow the refactor/rearchitect
-            pattern, ​and Infomerica's Architects, Cloud Specialists, and SecOps
-            Specialists work diligently to ensure your success in ​migrating to
-            AWS.
+        <div className="fs-6">
+          <div className="d-md-flex gap-3">
+            <p className="p-2 shadow rounded">
+              <b className="fs-5">Assess: </b>Infomerica initiates the migration
+              journey with a thorough Migration Readiness Assessment (MRA)
+              aligned ​with the AWS Cloud Adoption Framework. This assessment
+              evaluates the enterprise's cloud readiness and ​provides an action
+              plan to address identified gaps.
+            </p>
+            <p className="p-2 shadow rounded">
+              <b className="fs-5">Mobilise: </b>Infomerica builds AWS
+              proficiency within client organizations by facilitating hands-on
+              migration ​experiences. This phase focuses on establishing a
+              scalable and secure AWS landing zone while migrating select
+              ​business applications to the cloud.
+            </p>
+          </div>
+          <p className="p-2 shadow rounded">
+            <b className="fs-5">Migrate & Modernise: </b>A significant portion
+            (20-50%) of an enterprise application portfolio comprises repeated
+            ​patterns that can be optimized through a factory approach. So,
+            we're working with you to set up a Migration Factory ​using
+            patterns, processes, tools, resources, and methodologies defined and
+            tested during the mobilization phase to ​migrate applications at
+            scale. Larger and more complex applications often follow the
+            refactor/rearchitect pattern, ​and Infomerica's Architects, Cloud
+            Specialists, and SecOps Specialists work diligently to ensure your
+            success in ​migrating to AWS.
           </p>
         </div>
       </div>
@@ -214,25 +215,26 @@ function App() {
         <img
           src={MAPCustomerJourney}
           alt="MAP Customer Journey"
-          width="70%"
-          className="m-5"
+          width="60%"
+          className="m-2"
         />
       </div>
       <div className="m-5">
-        <h1 style={{ color: "#FE0000" }}>
-          Infomerica Provides the Migration Services
-        </h1>
-        <div className="row mt-5">
-          <div className="col-5">
-            <img
-              src={AWSMigrationServices}
-              alt="AWS Migration Services"
-              width="100%"
-            ></img>
-          </div>
-          <div className="col-7">
-            <div>
-              <h5>Migration Strategy Development:</h5>
+        <div className="d-md-flex gap-5 justify-content-center align-items-center">
+          <img
+            src={AWSMigrationServices}
+            alt="AWS Migration Services"
+            width="20%"
+          ></img>
+          <h1 className="d-md-flex justify-content-center align-items-center">
+            Infomerica Provides the Migration Services
+          </h1>
+        </div>
+
+        <div className="mt-5 d-md-flex gap-3">
+          <div>
+            <div className="m-2 p-2 shadow">
+              <h5>Migration Strategy Development</h5>
               <p>
                 Infomerica collaborates with clients to develop comprehensive
                 migration strategies aligned with their business objectives.
@@ -243,20 +245,8 @@ function App() {
                 allocation.
               </p>
             </div>
-            <div>
-              <h5>Workload Discovery and Assessment:</h5>
-              <p>
-                Infomerica thoroughly assesses existing applications and
-                workloads to evaluate their cloud migration readiness. This
-                process involves analyzing dependencies, performance
-                characteristics, data volumes, and compliance requirements. The
-                assessment helps identify potential migration challenges,
-                prioritize workloads, and develop plans tailored to each
-                workload's needs.
-              </p>
-            </div>
-            <div>
-              <h5>Migration Execution and Management:</h5>
+            <div className="bg-light m-2 p-2">
+              <h5>Migration Execution and Management</h5>
               <p>
                 Infomerica manages the end-to-end migration process, overseeing
                 workload migration, data migration, and application refactoring.
@@ -268,8 +258,8 @@ function App() {
                 smooth transition to the cloud.
               </p>
             </div>
-            <div>
-              <h5>Cloud Optimization and Cost Management:</h5>
+            <div className="m-2 p-2 shadow">
+              <h5>Cloud Optimization and Cost Management</h5>
               <p>
                 Post-migration, Infomerica optimizes AWS environments to enhance
                 performance, scalability, and cost efficiency. This involves
@@ -280,8 +270,22 @@ function App() {
                 optimize AWS spending while maintaining optimal performance.
               </p>
             </div>
-            <div>
-              <h5>Security and Compliance Assurance:</h5>
+          </div>
+          <div>
+            <div className="bg-light m-2 p-2">
+              <h5>Workload Discovery and Assessment</h5>
+              <p>
+                Infomerica thoroughly assesses existing applications and
+                workloads to evaluate their cloud migration readiness. This
+                process involves analyzing dependencies, performance
+                characteristics, data volumes, and compliance requirements. The
+                assessment helps identify potential migration challenges,
+                prioritize workloads, and develop plans tailored to each
+                workload's needs.
+              </p>
+            </div>
+            <div className="m-2 p-2 shadow">
+              <h5>Security and Compliance Assurance</h5>
               <p>
                 Infomerica prioritizes security and compliance throughout the
                 migration process, implementing robust security controls,
@@ -292,8 +296,8 @@ function App() {
                 and safeguard sensitive information.
               </p>
             </div>
-            <div>
-              <h5>Migration Tooling and Automation:</h5>
+            <div className="bg-light m-2 p-2">
+              <h5>Migration Tooling and Automation</h5>
               <p>
                 Infomerica leverages advanced migration tooling and automation
                 frameworks to streamline migration processes and reduce manual
@@ -309,154 +313,117 @@ function App() {
         </div>
       </div>
       <div>
-        <h1
-          style={{
-            backgroundColor: "rgb(14, 46, 78)",
-            color: "white",
-            padding: "20px",
-          }}
-        >
-          Infomerica: Empowering Seamless AWS Migration and Modernization
-        </h1>
-        <div>
+        <div className="d-md-flex">
           <div className="text-center">
             <img
               src={AWSMigrationModernization}
               alt="AWS Migration and Modernization"
+              width="60%"
             ></img>
           </div>
-          <div className="m-5">
-            <div>
-              <h5>Discovery, Planning, and Recommendation:</h5>
-              <p>
-                Infomerica assists in assessing your existing environment,
-                identifying workloads suitable for migration, and recommending
-                the best approach. Our expertise ensures faster and automated
-                application portfolio assessments, cutting down weeks of effort
-                needed for modernization assessments.
-              </p>
-            </div>
-            <div>
-              <h5>Business Case Analysis:</h5>
-              <p>
-                Infomerica helps build a compelling business case for
-                application modernization. For legacy workloads on AWS,
-                automated analysis with containers and serverless tech is
-                provided.
-              </p>
-            </div>
-            <div>
-              <h5>Application Mobility:</h5>
-              <p>
-                Infomerica offers tools for automated re-platforming,
-                refactoring, and augmented refactoring by developers. Our
-                services enable seamless migration and modernization of
-                applications.
-              </p>
-            </div>
-            <div>
-              <h5>Data Mobility:</h5>
-              <p>
-                Ensuring data remains accessible during migration is crucial.
-                Infomerica provides solutions for efficient data movement and
-                synchronization between on-premises systems and AWS.
-              </p>
-            </div>
-            <div>
-              <h5>App monitoring & composition:</h5>
-              <p>
-                During migration, monitoring and managing applications are
-                essential. Infomerica helps you manage and govern applications
-                while they are being modernized.
-              </p>
-            </div>
-          </div>
+          <h1 className="d-md-flex justify-content-center align-items-center">
+            Infomerica Empowering Seamless AWS Migration and Modernization
+          </h1>
         </div>
-      </div>
-      <div className="d-lg-flex bg-light">
-        <div className="col-lg-6">
-          <div className="footerBanner">
-            <div className="text-center">
-              <img
-                src={Logo}
-                alt="Logo"
-                width="30%"
-                style={{ marginTop: "30%" }}
-              ></img>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-6">
-          <h4 className="text-center pt-4">Contact Us</h4>
-          <hr className="text-danger w-75 footerHr" />
-          <div className="p-lg-4 p-3 d-flex gap-5">
-            <div className="fs-3">
-              <i className="bi bi-telephone-fill"></i>
-            </div>
-            <div>
-              <h4>PHONE</h4>
-              <h4>+1 919-655-0800</h4>
-            </div>
-          </div>
-          <div className="p-lg-4 p-3 d-flex gap-5">
-            <div className="fs-3">
-              <i className="bi bi-envelope-fill"></i>
-            </div>
-            <div>
-              <h4>EMAIL</h4>
-              <h4>
-                <a href="mailto:info@infomericainc.com" className="text-dark">
-                  info@infomericainc.com
-                </a>
-              </h4>
-            </div>
-          </div>
-          <div className="p-3 d-flex gap-5">
-            <h4>Follow Us</h4>
-            <div className="d-flex gap-4">
-              <div className="text-danger bg-danger fs-2 text-light ps-2 pe-2 rounded">
-                <a
-                  href="https://www.youtube.com/@infomericainc522"
-                  className="text-decoration-none text-light"
-                  target="blank"
-                >
-                  <i className="bi bi-youtube"></i>
-                </a>
-              </div>
-              <div className="text-info fs-2 bg-info ps-2 pe-2 rounded text-light">
-                <a
-                  href="https://www.linkedin.com/company/infomerica/"
-                  className="text-decoration-none text-light"
-                  target="blank"
-                >
-                  <i className="bi bi-linkedin"></i>
-                </a>
-              </div>
-              <div className="text-primary fs-2 bg-primary ps-2 pe-2 rounded text-light">
-                <a
-                  href="https://www.facebook.com/Infomerica1998/"
-                  className="text-decoration-none text-light"
-                  target="blank"
-                >
-                  <i className="bi bi-facebook"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="p-3">
+        <div className="m-5 d-md-flex gap-3">
+          <div className="p-2 shadow rounded">
+            <h5 className="text-center">Discovery, Planning, and Recommendation</h5>
             <p>
-              For more details or to discuss your specific needs,{" "}
-              <b><a
-                href="https://infomericainc.com/contact"
-                target="blank"
-                className="text-dark"
-              >
-                Contact Us
-              </a></b>{" "}
-              today!
+              Infomerica assists in assessing your existing environment,
+              identifying workloads suitable for migration, and recommending the
+              best approach. Our expertise ensures faster and automated
+              application portfolio assessments, cutting down weeks of effort
+              needed for modernization assessments.
+            </p>
+          </div>
+          <div className="p-2 shadow rounded">
+            <h5 className="text-center">Business Case Analysis</h5>
+            <p>
+              Infomerica helps build a compelling business case for application
+              modernization. For legacy workloads on AWS, automated analysis
+              with containers and serverless tech is provided.
+            </p>
+          </div>
+          <div className="p-2 shadow rounded">
+            <h5 className="text-center">Application Mobility</h5>
+            <p>
+              Infomerica offers tools for automated re-platforming, refactoring,
+              and augmented refactoring by developers. Our services enable
+              seamless migration and modernization of applications.
+            </p>
+          </div>
+          <div className="p-2 shadow rounded">
+            <h5 className="text-center">Data Mobility</h5>
+            <p>
+              Ensuring data remains accessible during migration is crucial.
+              Infomerica provides solutions for efficient data movement and
+              synchronization between on-premises systems and AWS.
+            </p>
+          </div>
+          <div className="p-2 shadow rounded">
+            <h5 className="text-center">App monitoring & composition</h5>
+            <p>
+              During migration, monitoring and managing applications are
+              essential. Infomerica helps you manage and govern applications
+              while they are being modernized.
             </p>
           </div>
         </div>
+      </div>
+      <div className="contactusdiv bg-light mt-3 mb-3">
+        <h1 className="text-center">Contact Us</h1>
+        <p className="text-center mt-3">
+          Please take a moment to get in touch, we will get back to you shortly.
+        </p>
+        <div className="d-md-flex p-2 ps-lg-5 pe-lg-5">
+          <div className="d-flex flex-column gap-3 col-md-6 p-2 p-lg-5">
+            <label>
+              <b>Your Name</b>
+            </label>
+            <input type="text" className="form-control" />
+            <label>
+              <b>Email Address</b>
+            </label>
+            <input type="email" className="form-control" />
+            <label>
+              <b>Phone Number</b>
+            </label>
+            <input type="tel" className="form-control" />
+            <label>
+              <b>Orgaization</b>
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="d-flex flex-column gap-3 col-md-6 p-2 p-lg-5">
+            <label>
+              <b>Message</b>
+            </label>
+            <textarea rows="10" cols="50" className="form-control"></textarea>
+            <a
+              href="mailto:info@infomericainc.com"
+              className="text-decoration-none text-white"
+            >
+              <button className="btn btn-danger fs-5 w-100">
+                Send Message
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="p-3 text-center">
+        <p>
+          For more details or to discuss your specific needs,{" "}
+          <b>
+            <a
+              href="https://infomericainc.com/contact"
+              target="blank"
+              className="text-dark"
+            >
+              Contact Us
+            </a>
+          </b>{" "}
+          today!
+        </p>
       </div>
     </>
   );
